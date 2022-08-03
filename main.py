f200d46cfdf3ac3dc1d675e62b7c8091f1926067
andrewhought <80929614+boolin9@users.noreply.github.com>
2022-08-03 14:08:52 -0700

    Convert txt file to json, added exception handling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 FONT_NAME = 'Helvetica'
 
@@ -10,17 +11,31 @@ def save_entry():
     website_text = website_entry.get()
     email_text = email_entry.get()
     password_text = password_entry.get()
-    if len(website_text) != 0 and len(email_text) != 0 and len(password_text) != 0:
-        is_correct = messagebox.askokcancel(message=f"Would you like to save this entry?:\n\nWebsite: {website_text}\nEmail: {email_text}\nPassword: {password_text}\n")
-        if is_correct:
-            file = open('password_bank.txt', 'a')
-            file.write(f"{website_text} | {email_text} | {password_text}\n")
-            file.close()
-            clear_text()
-    else:
-        messagebox.showinfo(title='Invalid entry', message='Please fill out all of the fields')
     
-
+    new_data = {
+        website_text: {
+            'email': email_text,
+            'password': password_text
+        }
+    }
+    
+    if len(website_text) == 0 or len(email_text) == 0 or len(password_text) == 0:
+        messagebox.showinfo(title='Invalid entry', message='Please fill out all of the fields')
+    else:
+        try:
+            with open('password_bank.json', 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open('password_bank.json', 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open('password_bank.json', 'w') as file:
+                json.dump(new_data, file, indent=4)
+        finally:
+            clear_text()
+            
+                    
 def clear_text():
     website_entry.delete(0, END)
     email_entry.delete(0, END)
@@ -43,7 +58,11 @@ def create_pass():
         
     password_entry.insert(0, password)
     pyperclip.copy(password)
+    
 
+def search_bank():
+    pass
+        
 
 win = Tk()
 win.title("Password Manager")
@@ -52,11 +71,14 @@ win.config(padx=20, pady=20)
 canv = Canvas(width=200 , height=200)
 logo = PhotoImage(file='passgen_img.png')
 canv.create_image(100, 100, image=logo)
-canv.grid(padx=(40, 0), pady=(0, 0), column=1, row=0)
+canv.grid(column=1, row=0, columnspan=2, padx=(0, 80))
 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=20)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
+
+search_button = Button(text='Search', width=11, command=search_bank)
+search_button.grid(column=2, row=1)
 
 website_label = Label(text='Website:', font=(FONT_NAME, 12, 'normal'))
 website_label.grid(column=0, row=1)
